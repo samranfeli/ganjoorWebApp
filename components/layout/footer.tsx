c95aad9c84ea53a4b496c7934f367e208673a321
samran <samran.f.m@gmail.com>
2026-06-20 22:35:11 +0330

    feat(footer): add navigation links

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,13 +1,31 @@
 import Container from "../ui/container";
+import { TextLink } from "../ui/text-link";
 
-export default function Footer(){
-    return(
-        <footer>
-            <Container>
-                <div className="py-5 border-t border-neutral-300">
-                    footer
-                </div>
-            </Container>
-        </footer>
-    )
+export default function Footer() {
+  const items: {
+    url: string;
+    label: string;
+  }[] = [
+    { url: "/", label: "خانه" },
+    { url: "/about", label: "معرفی" },
+    { url: "/faq", label: "پرسش‌های متداول" },
+    { url: "/privacy", label: "حریم خصوصی" },
+    { url: "/random", label: "بیت تصادفی" },
+    { url: "/blog", label: "تازه‌های گنجور" },
+    { url: "/contact", label: "تماس" },
+  ];
+
+  return (
+    <footer>
+      <Container>
+        <div className="py-5 border-t border-neutral-300 flex flex-wrap gap-5 justify-center text-sm">
+          {items.map((item) => (
+            <TextLink key={item.url} href={item.url}>
+              {item.label}
+            </TextLink>
+          ))}
+        </div>
+      </Container>
+    </footer>
+  );
 }
